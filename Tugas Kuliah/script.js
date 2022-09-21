6f321b39feb9fc8dee1ab44b8d914c93034564a5
RicoAlde46 <carolusricoaldeantoro@gmail.com>
2022-09-22 06:32:12 +0700

    Update File Baru

---
+++ Tugas Kuliah/script.js
@@ -0,0 +1,63 @@
+// function jumlahVolumeDuaKubus(a,b){
+//     var total;
+//     var volumeA;
+//     var volumeB;
+
+//     var volumeA = a * a * a;
+//     var volumeA = b * b * b;
+
+//     total = volumeA + volumeB  ;
+//     return total;
+//     }
+//      console.log(jumlahVolumeDuaKubus(8,3));
+//     alert(jumlahVolumeDuaKubus(8,3));
+//     alert(jumlahVolumeDuaKubus(10,15));
+// -----------------------------------------------
+// function tambah(a,b){
+//     return a + b;
+//     }
+//     var a = parseInt(prompt ('Masukkan Nilai 1 : '));
+//     var b= parseInt(prompt ('Masukkan Nilai 2 : '));
+//     console.log(tambah (a*2,b*2));
+// -------------------------------------------------------
+// function tambah(a, b) {
+//   return a + b;
+// }
+// function kali(a, b) {
+//   return a * b;
+// }
+// var coba = kali(tambah(1, 2), tambah(3, 4));
+// console.log(coba);
+// ------------------------------------------------------------
+// function tambah() {
+//   var hasil = 0;
+//   for (var i = 0; i < arguments.length; i++) {
+//     hasil += arguments[i];
+//   }
+//   return hasil;
+// }
+// var coba = tambah(1, 2, 3, 4);
+// console.log(coba);
+// --------------------------------------------------------    <script src="script.js"></script>
+// let text = "12345678";
+// const myArr = Array.from(text);
+
+// document.getElementById("demo").innerHTML = myArr;
+// ------------------------------------------------------
+// const fruits = ["RICO", "YUDHA", "AGUNG", "SAMUEL"];
+// let text = fruits.join();
+
+// document.getElementById("demo").innerHTML = text;
+// --------------------------------------------------------
+// Untuk HTML
+// <p>Click the button to demonstrate the prompt box.</p>
+// <button onclick="myFunction()">Try it</button>
+//    <p align="center" id="demo"></p>
+// <script src="script.js"></script>
+
+alert("Selamat Datang di Website Aldeantoro46");
+
+// let person = prompt("Please enter your name", "Please enter your name");
+// if (person != null) {
+//   document.getElementById("demo").innerHTML = "Hello " + person + "! How are you today? Thank you for visiting our website";
+// }
